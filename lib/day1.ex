a3f18fdc54e523625a838231ec3f546138ace8b9
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2023-12-03 21:09:31 +0800

    init

---
+++ lib/day1.ex
@@ -0,0 +1,22 @@
+defmodule Day1 do
+  # https://adventofcode.com/2023/day/1
+
+  defp input do
+    File.stream!("priv/day1.txt")
+    |> Enum.map(&String.trim/1)
+  end
+
+  @doc """
+      iex> part1()
+      nil
+  """
+  def part1 do
+  end
+
+  @doc """
+      iex> part2()
+      nil
+  """
+  def part2 do
+  end
+end
